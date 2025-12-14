404a80d8ce66d1e2751ec2b3652da1807b0fe86a
Dawit Mekonnen <56738450+Dave-lab12@users.noreply.github.com>
2025-12-14 15:49:13 +0300

    fix new drizzle instance instantiation on every request

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,8 @@ import { AppContext } from '$src/types'
 
 const app = new Hono<AppContext>()
 
+const db = drizzle(process.env.DATABASE_URL!, { schema })
 const dbMiddleware = createMiddleware(async (c, next) => {
-  const db = drizzle(process.env.DATABASE_URL!, { schema })
   c.set('db', db);
   c.set('schema', schema)
   await next();
@@ -42,4 +42,26 @@ app.get('/health', async (c) => {
   }
 })
 
+app.get('/extensions', async (c) => {
+  const db = c.get('db')
+  // SELECT * FROM pg_extension;
+  const result = await db.execute('SELECT * FROM pg_extension;');
+  return c.json({ extensions: result.rows });
+});
+
+app.get('/setup', async (c) => {
+  // install pgvector extension
+  const db = c.get('db');
+
+  // {"extensions":[{"oid":13564,"extname":"plpgsql","extowner":10,"extnamespace":11,"extrelocatable":false,"extversion":"1.0","extconfig":null,"extcondition":null},{"oid":24841,"extname":"vector","extowner":10,"extnamespace":2200,"extrelocatable":true,"extversion":"0.8.1","extconfig":null,"extcondition":null},{"oid":25169,"extname":"pg_trgm","extowner":10,"extnamespace":2200,"extrelocatable":true,"extversion":"1.6","extconfig":null,"extcondition":null},{"oid":25381,"extname":"fuzzystrmatch","extowner":10,"extnamespace":2200,"extrelocatable":true,"extversion":"1.2","extconfig":null,"extcondition":null}]}
+  try {
+    await db.execute('CREATE EXTENSION IF NOT EXISTS vector;');
+    await db.execute('CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;');
+    await db.execute('CREATE EXTENSION IF NOT EXISTS pg_trgm;');
+
+    return c.json({ message: 'Database extensions installed successfully' });
+  } catch (error) {
+    return c.json({ error: 'Failed to install database extensions' }, 500);
+  }
+})
 export default app
