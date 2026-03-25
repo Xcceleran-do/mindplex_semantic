75d37eeedd7144d9c2cfd5afb89e477ed9db6a7a
matiyassseifu@gmail.com <matiyassseifu@gmail.com>
2026-03-25 16:15:43 +0300

    fix : add max value validation for the maximum number of chunks to be returned

--- src/routes/chat/schema.ts
+++ src/routes/chat/schema.ts
@@ -3,7 +3,7 @@ import { IdParamSchema } from '$src/lib/validators';
 
 export const RetrieveChunksSchema = v.object({
   user_query: v.string(),
-  k: v.optional(v.number()),
+  k: v.optional(v.pipe(v.number(), v.maxValue(15))),
   filters: v.optional(
     v.object({
       article_id: v.optional(IdParamSchema),
